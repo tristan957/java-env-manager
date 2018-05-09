7d7d03419fd17bafb41b368c4008e226511243f5
Tristan Partin <tristan.partin@gmail.com>
2018-05-09 00:59:35 -0500

    matching madness

--- libjem/src/init.rs
+++ libjem/src/init.rs
@@ -24,13 +24,22 @@ pub fn init() -> Result<(), Box<Error>> {
     }
 
     program_dir.push("/settings.json");
-    let file = match fs::File::create(program_dir) {
-        Ok(f) => f,
+    match fs::File::create(program_dir) {
+        Ok(f) => match serde_json::to_writer_pretty(&f, &Settings::default()) {
+            Ok(_) => return Ok(()),
+            Err(e) => return Err(Box::new(e))
+        },
         Err(e) => return Err(Box::new(e))
-    };
-
-    match serde_json::to_writer_pretty(&file, &Settings::default()) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(Box::new(e))
     }
+
+    // program_dir.push("/settings.json");
+    // let file = match fs::File::create(program_dir) {
+    //     Ok(f) => f,
+    //     Err(e) => return Err(Box::new(e))
+    // };
+
+    // match serde_json::to_writer_pretty(&file, &Settings::default()) {
+    //     Ok(_) => Ok(()),
+    //     Err(e) => Err(Box::new(e))
+    // }
 }
